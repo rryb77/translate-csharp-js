e6754f49ed7db1eb47f72daf2027526b13ba595a
Ryan Youngblood <rryb77@gmail.com>
2021-02-26 15:02:00 -0600

    Enemies now mimic the C# version

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,8 +1,57 @@
-let response = prompt("Who would you like to say hello to?")
+console.log("My Enemies List!")
+console.log("----------------")
 
+const getEnemies = () => {
+    let currentEnemies = [
+        {
+            firstName: "Joshua",
+            lastName: "Flowers",
+            offenses: [
+                "Being a jerk to me in elementary school",
+                "Not being nice to me in elementary school"
+            ],
+            isReallyHated: true
+        },
+        {
+            firstName: "Darth",
+            lastName: "Vader",
+            offenses: [
+                "Cut off Luke's hand",
+                "Murdered all those kids",
+                "Unkind management practices"
+            ],
+            isReallyHated: false
+        },
+        {
+            firstName: "Betty",
+            lastName: "Rudelady",
+            offenses: [
+                "Phone calls in the theater",
+                "Phone calls on the bus",
+                "Phone calls in line at the grocery store",
+                "Poor conversationalist"
+            ],
+            isReallyHated: true
+        },
+        {
+            firstName: "Leon",
+            lastName: "Peck",
+            offenses: [
+                "Keeps giving me a hotplate"
+            ],
+            isReallyHated: false
+        }
+    ]
 
-if (response === ""){
-    console.log("Fine, don't say 'hello'!")
-} else {
-    console.log(`Hello, ${response}`)
+    return currentEnemies
+}
+
+let enemies = getEnemies()
+
+for (myEnemy of enemies){
+    if(myEnemy.isReallyHated === true){
+        console.log(`${myEnemy.firstName} ${myEnemy.lastName} (Really, really dislike!)`)
+    } else {
+        console.log(`${myEnemy.firstName} ${myEnemy.lastName}`)
+    }
 }
