ce2558b06e4bf8ea190bcfb742449bc53467c485
Ryan Youngblood <rryb77@gmail.com>
2021-02-26 14:23:46 -0600

    Enemies now closely mimics the C# version

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,8 @@
-// Put your code here
+let response = prompt("Who would you like to say hello to?")
+
+
+if (response === ""){
+    console.log("Fine, don't say 'hello'!")
+} else {
+    console.log(`Hello, ${response}`)
+}
