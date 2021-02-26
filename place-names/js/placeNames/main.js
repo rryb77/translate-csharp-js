2391905fd06158859d38b57f08e2d070e7f924b6
Ryan Youngblood <rryb77@gmail.com>
2021-02-26 14:12:50 -0600

    Place names now mimics the C# version

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,19 @@
-// Put your code here
+// Setup the array of names
+let names = ["Nashville", "Hong Kong", "The back yard", "Earth", "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+// Print some output to the console
+console.log("All Place Names")
+
+for (theName of names){
+    console.log(theName)
+}
+
+console.log("")
+
+let filteredNames = names.filter(n => n.startsWith("The"))
+
+console.log("'The' Place Names")
+
+for (theName of filteredNames) {
+    console.log(theName)
+}
