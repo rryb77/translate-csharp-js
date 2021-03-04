11ba0334e3351bde027ad308c71fce95ee50ffd0
Ryan Youngblood <rryb77@gmail.com>
2021-03-04 15:28:32 -0600

    initial commit

---
+++ insulter/csharp/insulter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace insulter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ wish-list/csharp/wish-list/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wish_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
