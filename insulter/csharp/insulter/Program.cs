999604817b0b3b52bee7b9585aedf06c252347ba
Ryan Youngblood <rryb77@gmail.com>
2021-03-04 15:41:24 -0600

    The insulter JS project has been converted to C#

--- insulter/csharp/insulter/Program.cs
+++ insulter/csharp/insulter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace insulter
 {
@@ -6,7 +7,33 @@ namespace insulter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> insults = new List<string>()
+            {
+                "You look like what morning breath smells like.",
+                "If you tried to give me cpr I would probably throw myself back under water",
+                "I am not a fan of you",
+                "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
+                "I'd rather walk than be on the same plane as you",
+                "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
+                "Do you have to be so...like that?"
+            };
+
+            List<int> indexes = new List<int>();
+
+            while (indexes.Count < 3)
+            {
+                int candidate = new Random().Next(insults.Count - 1);
+                if (!indexes.Contains(candidate))
+                {
+                    indexes.Add(candidate);
+                }
+            }
+
+            for (int i = 0; i < indexes.Count; i++)
+            {
+                int index = indexes[i];
+                Console.WriteLine(insults[index]);
+            }
         }
     }
 }
