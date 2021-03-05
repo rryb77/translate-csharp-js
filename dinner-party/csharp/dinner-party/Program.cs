44b07047c6fe8ae276e46b63cb085e3dccef3c1a
Ryan Youngblood <rryb77@gmail.com>
2021-03-05 11:02:07 -0600

    intial commit

---
+++ dinner-party/csharp/dinner-party/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dinner_party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
